053b79d987847864f3679c6774fda519a0f69fc5
Allzer <109965453+Allzer@users.noreply.github.com>
2024-02-14 15:35:20 +0300

    Дополнил парсер

--- parser.py
+++ parser.py
@@ -1,19 +1,27 @@
-import json
-
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('https://', adapter)
+url = "https://github.com/trending?since=monthly"
+
 
 # Загрузка страницы
-url = "https://github.com/trending?since=monthly"
-response = requests.get(url)
+response = session.get(url)
 html = response.text
 
 # Извлечение информации
 soup = BeautifulSoup(html, "html.parser")
 repos = soup.find_all("article", class_="Box-row")
 
+counter = 0
 # Вывод информации о репозиториях
 for repo in repos[:1]:
+    counter += 1
     # Название репозитория
     repo_name_elem = repo.find("h2", class_="h3 lh-condensed")
     if repo_name_elem:
@@ -38,11 +46,51 @@ for repo in repos[:1]:
     if owners:
         owner_str = ", ".join(owners)
     else:
-        owner_str = "Owner not found"
+        owner_str = "Владелец не найден"
 
     del owners[:2]
+
+    #Количество форков
+    forks_elem = repo.find("a", class_="Link--muted", href=lambda href: href and "/forks" in href)
+    if forks_elem:
+        forks_text = forks_elem.text.strip().replace(",","")
+    else:
+        forks_text = "Колчиество форков не найдено"
+
+    # Количество открытых issues
+    issues_elem = repo.find("a", class_="Link--muted", href=lambda href: href and "/issues" in href)
+    if issues_elem:
+        issues_text = issues_elem.text.strip()
+    else:
+        issues_text = "Issues count not found"
+
+    #Количество просмотров
+    # Загрузка страницы
+    url_2 = f'https://github.com/{repo_name}'
+    response_2 = session.get(url_2)
+    html_2 = response_2.text
+
+    # Извлечение информации
+    soup_2 = BeautifulSoup(html, "html.parser")
+    repos_2 = soup.find_all("div", class_="mt-2")
+
+    for repo2 in repos_2:
+        views_elem = repo2.find("span", class_="Link--muted", recursive=False)
+        if views_elem:
+            views_text = views_elem.text.strip()
+        else:
+            views_text = "Views count not found"
+
+
+
 
 
     print("Repository:", repo_name)
     print("Stars:", stars)
     print("Owners", owners)
+    print("Position", counter)
+    print("Watchers")
+    print("Forks", forks_text)
+    print("Issues", issues_text)
+    print("Views", views_text)
+
