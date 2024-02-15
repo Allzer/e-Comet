e038a2ac9708ae73e93f388efc0bd48c6a151d47
Allzer <109965453+Allzer@users.noreply.github.com>
2024-02-15 20:31:24 +0400

    Окончательная версия парсера
    Проверить, как работает на другом компьютере

--- parser.py
+++ parser.py
@@ -2,52 +2,73 @@ import requests
 from bs4 import BeautifulSoup
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
+import re
 
 
 url = "https://github.com/EvanLi/Github-Ranking/blob/master/Top100/Top-100-stars.md"
-def parser(url):
-    #Обход ограничения запросов к старнице
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('https://', adapter)
+#Обход ограничения запросов к старнице
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('https://', adapter)
 
 
-    # Загрузка страницы
-    response = session.get(url)
-    html = response.text
+# Загрузка страницы
+response = session.get(url)
+html = response.text
 
-    # Извлечение информации
-    soup = BeautifulSoup(html, "html.parser")
-    repos = soup.find_all("tbody")
+# Извлечение информации
+soup = BeautifulSoup(html, "html.parser")
+repos = soup.find_all("tr")
 
-    for repo in repos[:1]:
-        rait = int(repo.find_all("td")[0].text)
-        name = repo.find_all("td")[1].find("a")["href"].replace('https://github.com/',"").replace('"', "").replace("\\",'')
-        stars = int(repo.find_all("td")[2].text)
-        forks = int(repo.find_all("td")[3].text)
-        issues = int(repo.find_all("td")[5].text)
-        language = repo.find_all("td")[4].text
+for repo in repos:
+    rait = repo.find_all("td")[0:1]
+    for i in rait:
+        rait = int(i.text)
+
+    name = repo.find_all("td")[1:2]
+    for i in name:
+        name = i.find("a")['href'].replace('https://github.com/',"").replace('"', "").replace("\\",'')
+
+    stars = repo.find_all("td")[2:3]
+    for i in stars:
+        stars = int(i.text)
+
+    forks = repo.find_all("td")[3:4]
+    for i in forks:
+        forks = int(i.text)
+    issues = repo.find_all("td")[5:6]
+    for i in issues:
+        issues = int(i.text)
+    language = repo.find_all("td")[4:5]
+    for i in language:
+        language = i.text
+
+    """количество просмотров и подключение"""
+    url_page = f"https://github.com/{name}"
+    response_page = session.get(url_page)
+    html_page = response_page.text
+
+    """Извлечение информации о просмотрах"""
+
+    soup_page = BeautifulSoup(html_page, "html.parser")
+    views = soup_page.find_all("a", href=f"/{name}/watchers")
+    for i in views:
+        views = i.text.replace("\n", "").replace("watching", "").replace(" ", "")
+        if "k" in views:
+            views = int(float(views.replace("k",""))*1000)
 
 
-        """количество просмотров и подключение"""
-        url_page = f"https://github.com/{name}"
-        response_page = session.get(url_page)
-        html_page = response_page.text
-
-        """Извлечение информации о просмотрах"""
-        soup_page = BeautifulSoup(html_page, "html.parser")
-        views = int(soup_page.find("a", href=f"/{name}/watchers").text.replace("\n", "").replace("watching", "").replace("k","00").replace(" ","").replace(".",""))
 
 
 
 
-        """Извлечение информации о владельцах"""
-        owner_link = soup_page.find_all("li", class_='mb-2 mr-2')
-        owners = []
-        for i in range(len(owner_link)):
-            owners.append(owner_link[i].find("a")["href"].replace("https://github.com/",""))
 
+    """Извлечение информации о владельцах"""
+    owner_link = soup_page.find_all("li", class_='mb-2 mr-2')
+    owners = []
+    for i in range(len(owner_link)):
+        owners.append(owner_link[i].find("a")["href"].replace("https://github.com/", ""))
         dict = {
             "repo":name,
             "owner":owners,
@@ -59,18 +80,6 @@ def parser(url):
             "open_issues":issues,
             "language":language
         }
+    print(dict)
 
-        return dict
 
-        #print("Название", name)
-        #print("Владелец", owners)
-        #print("Рейтинг", rait)
-        #print("Предыдущая позиция в топе", rait-1)
-        #print("Кол-во звёзд", stars)
-        #print("Кол-во просмотров", views)
-        #print("Кол-во форков", forks)
-        #print("Кол-во issues", issues)
-        #print("Язык", language)
-        #print("")
-
-print(parser(url))
